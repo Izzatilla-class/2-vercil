de2393469cf46359e2a794a570c1da42802af840
Izzatilla-class <izzatillaxolmatjonov277@gmail.com>
2026-05-15 10:10:55 +0500

    first commit

---
+++ scrip.js
@@ -0,0 +1,14 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+document.addEventListener("keydown" function( event) {
+    jump();
+});
+
+function jump () {
+if (dino.classList !="jump")  {
+    dino.classList.add ("jump")
+}
+setTimeout(function)(
+
+),300}
